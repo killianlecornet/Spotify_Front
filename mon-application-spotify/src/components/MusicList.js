7145efbd32bec330fec71a43745849e0af392a7c
killianlecornet <56248842+killianlecornet@users.noreply.github.com>
2023-11-26 17:01:43 +0100

    regroupement des musiques dans leur artistes pour apres écouter la musique

--- mon-application-spotify/src/components/MusicList.js
+++ mon-application-spotify/src/components/MusicList.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import MusicPlayer from './MusicPlayer'; // Assurez-vous d'avoir ce composant
+import MusicPlayer from './MusicPlayer'; // Assurez-vous que ce composant existe et fonctionne correctement
 
 function MusicList() {
     const [musics, setMusics] = useState([]);
+    const [selectedArtist, setSelectedArtist] = useState(null);
+    const [currentMusicUrl, setCurrentMusicUrl] = useState('');
 
     useEffect(() => {
         // Remplacer par l'URL de votre API
@@ -12,28 +14,41 @@ function MusicList() {
             .catch(error => console.error('Erreur:', error));
     }, []);
 
+    const groupByArtist = (musicsArray) => {
+        return musicsArray.reduce((acc, music) => {
+            acc[music.artist] = [...(acc[music.artist] || []), music];
+            return acc;
+        }, {});
+    };
+
+    const musicsByArtist = groupByArtist(musics);
+
+    const handleMusicClick = (url) => {
+        setCurrentMusicUrl(url);
+    };
+
     return (
         <div>
-            <h1>Liste des Musiques</h1>
-            {musics.map(music => (
-                <MusicItem key={music._id} music={music} />
-            ))}
-        </div>
-    );
-}
-
-function MusicItem({ music }) {
-    const [showDetails, setShowDetails] = useState(false);
-
-    return (
-        <div>
-            <h3 onClick={() => setShowDetails(!showDetails)}>{music.title}</h3>
-            {showDetails && (
+            <h1>Liste des Artistes</h1>
+            {selectedArtist === null ? (
+                Object.keys(musicsByArtist).map(artist => (
+                    <h3 key={artist} onClick={() => setSelectedArtist(artist)}>
+                        {artist}
+                    </h3>
+                ))
+            ) : (
                 <div>
-                    <p>{music.artist}</p>
-                    <MusicPlayer url={music.url} />
+                    <button onClick={() => setSelectedArtist(null)}>Retour</button>
+                    <h2>Musiques de {selectedArtist}</h2>
+                    {musicsByArtist[selectedArtist].map(music => (
+                        <div key={music._id}>
+                            <h4 onClick={() => handleMusicClick(music.url)}>{music.title}</h4>
+                        </div>
+                    ))}
                 </div>
             )}
+
+            {currentMusicUrl && <MusicPlayer url={currentMusicUrl} />}
         </div>
     );
 }
