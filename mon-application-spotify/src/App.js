0f4265ed0e6c46dfb8c0133d2cb7b185b51d153f
unknown <matthiascrespo9@gmail.com>
2023-12-17 02:46:55 +0100

    Utilisation du routeur grace a zak

--- mon-application-spotify/src/App.js
+++ mon-application-spotify/src/App.js
@@ -1,59 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import MusicList from './components/MusicList/MusicList';
-import FavoritesPage from './components/FavoritesPage/FavoritesPage';
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook, faHome, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import ArtistList from './components/ArtistList/ArtistList';
-import AlbumList from './components/AlbumList/AlbumList';
-// import MusicForm from './components/MusicForm/MusicForm';
-// import Search from './components/SearchByArtist';
+import { RouterProvider } from "react-router-dom";
+import router from "./config/router";
 
-function App() {
-    const [musics, setMusics] = useState([]);
-    const [favorites, setFavorites] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:3001/api/musics')
-            .then(response => response.json())
-            .then(data => setMusics(data))
-            .catch(error => console.error('Erreur:', error));
-    }, []);
-
-    const toggleFavorite = (musicId) => {
-        setFavorites(favorites.includes(musicId)
-            ? favorites.filter(id => id !== musicId)
-            : [...favorites, musicId]);
-    };
-
-    return (
-        <div className="App">
-            <div className='leftNavbar'>
-                <div className='topLeft'>
-                    <div className='iconMargin'>
-                        <FontAwesomeIcon icon={faHome} /> Maison
-                    </div>
-                    <div className='iconMargin'>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} /> Recherche
-                    </div>
-                </div>
-
-                <div className='bottomLeft'>
-                    <div className='iconMargin'>
-                        <FontAwesomeIcon icon={faBook} /> bibliotheque
-                    </div>
-                    {/* <MusicList musics={musics} toggleFavorite={toggleFavorite} favorites={favorites} /> */}
-                </div>
-            </div>
-
-            <div className='mainInfos'>
-                <AlbumList/>
-                <ArtistList/>
-                {/* <MusicList musics={musics} toggleFavorite={toggleFavorite} favorites={favorites} /> */}
-                {/* <FavoritesPage favorites={favorites} musics={musics} /> */}
-            </div>
-        </div>
-    );
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
---
+++ mon-application-spotify/src/pages/Home/Home.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import MusicList from '../../components/MusicList/MusicList';
+import FavoritesPage from '../../components/FavoritesPage/FavoritesPage';
+import './Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook, faHome, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import ArtistList from '../../components/ArtistList/ArtistList';
+import AlbumList from '../../components/AlbumList/AlbumList';
+// import MusicForm from './components/MusicForm/MusicForm';
+// import Search from './components/SearchByArtist';
+
+function Home() {
+    const [musics, setMusics] = useState([]);
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/musics')
+            .then(response => response.json())
+            .then(data => setMusics(data))
+            .catch(error => console.error('Erreur:', error));
+    }, []);
+
+    const toggleFavorite = (musicId) => {
+        setFavorites(favorites.includes(musicId)
+            ? favorites.filter(id => id !== musicId)
+            : [...favorites, musicId]);
+    };
+
+    return (
+        <div className="App">
+            <div className='leftNavbar'>
+                <div className='topLeft'>
+                    <div className='iconMargin'>
+                        <FontAwesomeIcon icon={faHome} /> Maison
+                    </div>
+                    <div className='iconMargin'>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} /> Recherche
+                    </div>
+                </div>
+
+                <div className='bottomLeft'>
+                    <div className='iconMargin'>
+                        <FontAwesomeIcon icon={faBook} /> bibliotheque
+                    </div>
+                    {/* <MusicList musics={musics} toggleFavorite={toggleFavorite} favorites={favorites} /> */}
+                </div>
+            </div>
+
+            <div className='mainInfos'>
+                <AlbumList/>
+                <ArtistList/>
+                {/* <MusicList musics={musics} toggleFavorite={toggleFavorite} favorites={favorites} /> */}
+                {/* <FavoritesPage favorites={favorites} musics={musics} /> */}
+            </div>
+        </div>
+    );
+}
+
+export default Home;
